dd1f0b4f6ab36edf5c87d0932a09b686c9a85534
SumanBalayar <orthohydrogen123@gmail.com>
2023-10-17 18:17:29 +0545

    footer fix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,16 @@
 import React from "react";
-import {CiTwitter,CiFacebook,CiInstagram,CiLinkedin} from 'react-icons/ci'
-
+import { CiTwitter, CiFacebook, CiInstagram, CiLinkedin } from "react-icons/ci";
 
 const Footer = () => {
   return (
-    <div className="flex px-[7%] text-white bg-zinc-800 py-[6%] justify-between">
-      <div className="flex-col justify-between w-80 space-y-24">
-        <div className="flex-col space-y-4">
-          <div className="text-2xl font-bold">SUMAN BALAYAR</div>
-          <div className="text-sm ">
+    <div className="text-white bg-zinc-800">
+      <div className="flex-1 md:flex md:justify-around md:px-[2%] md:py-[6%]">
+        <div className="flex-col md:w-96 md:space-y-4">
+          <div className="text-2xl font-bold items-center justify-center py-[6%] md:py-0 text-center md:text-left">
+            SUMAN BALAYAR
+          </div>
+          <hr className="md:hidden border-slate-600 border-1" />
+          <div className="text-sm pt-[6%] px-[4%] md:pt-0 md:px-0 text-center md:text-left">
             Mark is the three-time #1 New York Times bestselling author of The
             Subtle Art of Not Giving a F*ck, as well as other titles. His books
             have sold around 20 million copies, been translated into more than
@@ -16,36 +18,39 @@ const Footer = () => {
             In 2023, a feature film about his life and ideas was released
             worldwide by Universal Pictures.
           </div>
-          <div className="font-semibold text-md">LEARN MORE ABOUT MARK</div>
+          <div className="font-semibold text-md py-[4%] md:py-0 text-center md:text-left">
+            LEARN MORE ABOUT MARK
+          </div>
         </div>
-        <div>© 2023 Infinity Squared Media LLC</div>
-      </div>
-      <hr className="border-red-600" />
-      <div className="flex flex-col justify-between">
-        <div className="flex space-x-40">
-          <ul className="flex-col space-y-4 font-serif">
-            <li>ARTICLES</li>
-            <li>COURSES</li>
-            <li>ABOUT SUMAN</li>
-            <li>CONTACT</li>
-          </ul>
-          <ul className="flex-col space-y-4 font-serif">
-            <li>BOOK</li>
-            <li>NEWSLETTER</li>
-            <li>SIGN UP</li>
-          </ul>
-        </div>
-        <div className="flex space-x-40">
-          <ul className="flex flex-col text-sm font-serif space-y-2">
-            <li>Privacy Policy</li>
-            <li>Cookies Settings</li>
-            <li>Terms and Conditions</li>
-          </ul>
-          <div className="flex space-x-5">
-            <CiTwitter className="scale-150"/>
-            <CiFacebook className="scale-150"/>
-            <CiInstagram className="scale-150"/>
-            <CiLinkedin className="scale-150"/>
+        <hr className="md:hidden border-slate-600 border-1" />
+        <div>
+          <div className="flex space-x-20 justify-center py-[6%]">
+            <ul className="flex-col space-y-4 font-serif">
+              <li>ARTICLES</li>
+              <li>COURSES</li>
+              <li>ABOUT SUMAN</li>
+              <li>CONTACT</li>
+            </ul>
+            <ul className="flex-col space-y-4 font-serif">
+              <li>BOOK</li>
+              <li>NEWSLETTER</li>
+              <li>SIGN UP</li>
+            </ul>
+          </div>
+          <hr className="md:hidden border-slate-600 border-1" />
+          <div className="flex-col flex md:flex-row-reverse items-center justify-center space-y-8 py-[6%]">
+            <div className="flex space-x-2 items-center justify-center md:-translate-y-5">
+              <CiFacebook size={30} />
+              <CiInstagram size={30} />
+              <CiLinkedin size={30} />
+              <CiTwitter size={30} />
+            </div>
+            <ul className="flex-col space-y-4 font-serif text-center md:text-left">
+              <li>Privacy Policy</li>
+              <li>Manage Cookie Settings</li>
+              <li>Terms and Conditions</li>
+            </ul>
+            <div className="text-center md:hidden">© 2023 Infinity Squared Media LLC</div>
           </div>
         </div>
       </div>
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -6,33 +6,94 @@ const Home = () => {
   return (
     <>
       <div className="bg-slate-200">
-        <div className="flex flex-col items-center justify-center py-[4%] px-[2%]">
-          <div className="md:flex">
-            <span className="text-8xl font-semibold text-center">
+        <div className="flex flex-col items-center justify-center py-[4%] px-[2%] space-y-10">
+          <div className="md:flex text-center">
+            <span className="text-5xl md:text-6xl lg:text-7xl font-semibold">
               5 Minutes That Might
               <br /> Change Your Life
             </span>
           </div>
 
-          <div className="flex flex-row items-center justify-center h-fit">
-            <div>
+          <div className="flex-1 md:flex items-center justify-center h-fit">
+            <div className="flex items-center justify-center">
               <img
-                className="scale-[75%]"
+                className="w-2/3"
                 src={image1}
                 alt="Phone Image"
               />
             </div>
             <div className="flex flex-col space-y-6 mt-5">
-              <div className="text-2xl font-extrabold">
+              <div className="text-xl lg:text-2xl font-extrabold text-center md:text-left">
                 5921 PEOPLE HAVE SHARED
                 <br /> BREAKTHROUGHS SO FAR. WILL THE
                 <br /> NEXT ONE BE YOU?
               </div>
-              <div className="text-black font-serif text-md">
+              <div className="text-black font-serif text-md text-center md:text-left">
                 Receive ideas shared with millions of people each week directly
                 to your inbox.
               </div>
-              <div className="flex relative">
+
+
+              <div className="flex-1 md:flex space-y-2 md:space-y-0 md:relative ">
+                <input
+                  placeholder="Enter Email Address"
+                  className="rounded-full py-3 px-2 w-full border-black border-solid border-2"
+                ></input>
+                <button className="md:absolute py-3 px-2 w-full md:w-auto rounded-full bg-black text-white hover:bg-orange-600 hover:text-black cursor-pointer md:right-0 md:translate-y-0.5">
+                  Sign Up
+                </button>
+              </div>
+
+
+              <div>
+                <div className="text-sm italic font-light text-center md:text-left">
+                  Your information is protected and I never spam, ever. You can
+                  view my privacy policy here.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="border-black" />
+      </div>
+
+
+      <div className="bg-white space-y-8 text-black flex-col items-center justify-center py-[10%] px-[2%]">
+        <div className="items-center flex justify-center text-3xl lg:text-4xl font-bold">
+          Hi, I'm Suman Balayar
+        </div>
+        <div className="w-50 text-xl lg:text-2xl font-sans text-center px-[5%] md:px-[10%] lg:px-[20%]">
+          I am the #1 NY Times Bestselling author of The Subtle Art of Not
+          Giving a F*ck and Everything Is F*cked: A Book About Hope. I write
+          life advice that is science-based, pragmatic, and
+          non-bullshitty—a.k.a., life advice that doesn’t suck. Some people say
+          I’m an idiot. Other people say I saved their life. Read on and decide
+          for yourself.
+        </div>
+        <div className="flex flex-row justify-center space-x-4 text-sm md:text-base">
+          <button className="bg-gray-900 text-white rounded-full px-4 py-3 md:px-9 md:py-4">
+            READ MY ARTICLES
+          </button>
+          <button className="bg-gray-900 text-white rounded-full px-4 py-3 md:px-9 md:py-4">
+            TAKE MY COURSES
+          </button>
+        </div>
+      </div>
+      <hr className="border-black" />
+
+
+      <div className="flex flex-col-reverse md:flex-row px-[2%] py-[10%] justify-center bg-orange-600">
+        <div className="flex flex-col space-y-5 ">
+          <div className="font-bold text-lg text-center md:text-left">5 MINUTES EACH WEEK THAT MIGHT CHANGE YOUR <br/>LIFE</div>
+          <div className="text-lg font-serif text-center md:text-left">
+            Enter your email address below to sign up for my free newsletter,
+            Your Next Breakthrough. Each week, you’ll receive a few prompts and
+            exercises designed to create your next breakthrough. No fluff, no
+            filler, no BS. Just five minutes each week that might change your
+            life.
+          </div>
+
+          <div className="flex relative">
                 <input
                   placeholder="Enter Email Address"
                   className="rounded-full py-3 px-2 w-full border-black border-solid border-2"
@@ -41,67 +102,19 @@ const Home = () => {
                   Sign Up
                 </button>
               </div>
-              <div>
-                <span className="text-sm italic font-light">
-                  Your information is protected and I never spam, ever. You can
-                  view my privacy policy here.
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr className="border-black" />
-      </div>
-      <div className="bg-white space-y-8 text-black flex-col items-center justify-center my-[10%] px-[4%]">
-        <div className="items-center flex justify-center text-4xl font-bold">
-          Hi, I'm Suman Balayar
-        </div>
-        <div className="w-50 text-2xl font-sans text-center px-52">
-          I am the #1 NY Times Bestselling author of The Subtle Art of Not
-          Giving a F*ck and Everything Is F*cked: A Book About Hope. I write
-          life advice that is science-based, pragmatic, and
-          non-bullshitty—a.k.a., life advice that doesn’t suck. Some people say
-          I’m an idiot. Other people say I saved their life. Read on and decide
-          for yourself.
-        </div>
-        <div className="flex flex-row justify-center space-x-4">
-          <button className="bg-gray-900 text-white rounded-full px-9 py-4">
-            READ MY ARTICLES
-          </button>
-          <button className="bg-gray-900 text-white rounded-full px-9 py-4">
-            TAKE MY COURSES
-          </button>
-        </div>
-      </div>
-      <hr className="border-black" />
-      <div className="flex px-[7%] justify-center bg-orange-600">
-        <div className="flex flex-col space-y-5 my-[10%]">
-          <div className="font-bold text-2xl">5 MINUTES EACH WEEK THAT MIGHT CHANGE YOUR <br/>LIFE</div>
-          <div className="text-lg font-serif">
-            Enter your email address below to sign up for my free newsletter,
-            Your Next Breakthrough. Each week, you’ll receive a few prompts and
-            exercises designed to create your next breakthrough. No fluff, no
-            filler, no BS. Just five minutes each week that might change your
-            life.
-          </div>
-          <div className="relative flex">
-            <input
-              placeholder="Enter Email Address"
-              className="rounded-full py-3 px-2 w-full border-2 border-solid border-black"
-            ></input>
-            <button className="py-3 px-5 rounded-full text-white bg-black hover:bg-orange-600 hover:text-black absolute top-0 right-0 translate-y-0.5">
-              Sign Up
-            </button>
-          </div>
-          <div className="text-sm italic font-light">
+
+          <div className="text-sm italic font-light text-center md:text-left">
             Your information is protected and I never spam, ever. You can view
             my privacy policy here.
           </div>
         </div>
-        <div>
-          <img className=" scale-[75%]" src={image2} alt="Slanted Phone"/>
+        <div className="flex items-center justify-center">
+          <img className="w-1/3 md:w2/3" src={image2} alt="Slanted Phone"/>
         </div>
       </div>
+
+
+
     </>
   );
 };
